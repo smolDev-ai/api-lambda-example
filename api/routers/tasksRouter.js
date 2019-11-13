f2758e73f78035520c09359a4cd018666dcce3f1
Megan <megan.miller@smoldev.dev>
2019-11-12 18:54:56 -0600

    setting up

--- api/routers/tasksRouter.js
+++ api/routers/tasksRouter.js
@@ -3,11 +3,11 @@ const router = require("express").Router();
 const Tasks = require("../helpers/tasksModel");
 
 router.get("/", (req, res) => {
-  Tasks.find()
-    .then(tasks => {
-      res.status(200).json(tasks);
-    })
-    .catch(err => res.status(500).json(err));
+	Tasks.find()
+		.then(tasks => {
+			res.status(200).json(tasks);
+		})
+		.catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
