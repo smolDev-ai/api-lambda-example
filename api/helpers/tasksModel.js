1ebeb34fc8c95fee46beb0d30c72391561d15ebb
Megan <megan.miller@smoldev.dev>
2019-11-12 19:35:01 -0600

    Created an add function and post route

--- api/helpers/tasksModel.js
+++ api/helpers/tasksModel.js
@@ -1,15 +1,25 @@
 const db = require("../../data/dbConfig");
 
 module.exports = {
-  find
+	find,
+	add,
 };
 
 function find(id) {
-  if (id) {
-    return db("tasks")
-      .where({ id })
-      .first();
-  } else {
-    return db("tasks");
-  }
+	if (id) {
+		return db("tasks")
+			.where({ id })
+			.first();
+	} else {
+		return db("tasks");
+	}
+}
+
+function add(item) {
+	return db("tasks")
+		.insert(item, "id")
+		.then(ids => {
+			const [id] = ids;
+			return find(id);
+		});
 }
--- api/routers/tasksRouter.js
+++ api/routers/tasksRouter.js
@@ -10,4 +10,14 @@ router.get("/", (req, res) => {
 		.catch(err => res.status(500).json(err));
 });
 
+router.post("/", (req, res) => {
+	Tasks.add(req.body)
+		.then(task => {
+			res.status(201).json(task);
+		})
+		.catch(err => {
+			res.status(500).json(err);
+		});
+});
+
 module.exports = router;
